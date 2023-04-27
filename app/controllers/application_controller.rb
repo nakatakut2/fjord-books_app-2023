8ea96ae95efb43aa8c950cf65f5e84f9810f05a8
nakatakut2 <nakatakut2@gamil.com>
2023-04-27 14:48:23 +0900

    既存ファイルで変更になったもの

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # 新規登録時に保存を許可するカラムを追加する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # 編集したデータをアップデートする時に保存を許可するカラムを追加する
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name postal_code address profile])
+  end
 end
