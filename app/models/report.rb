86a4910f32e2e271b034a6beceae4a9d0d8392e2
nakatakut2 <nakatakut2@gamil.com>
2023-06-03 11:57:27 +0900

    true/falseに一貫性を持たせる

--- app/models/report.rb
+++ app/models/report.rb
@@ -18,20 +18,12 @@ class Report < ApplicationRecord
     created_at.to_date
   end
 
-  def find_ids
-    content.scan(%r{http://localhost:3000/reports/(\d+)}).uniq.flatten.map(&:to_i)
-  end
-
   def save_with_mentions
     success = false
     ActiveRecord::Base.transaction do
-      raise ActiveRecord::Rollback unless save
-
-      find_ids.each do |mentioned_id|
-        raise ActiveRecord::Rollback unless Mention.create(mentioning_report_id: id, mentioned_report_id: mentioned_id)
-
-        success = true
-      end
+      success = save
+      success &= save_mentions
+      raise ActiveRecord::Rollback unless success
     end
     success
   end
@@ -39,29 +31,26 @@ class Report < ApplicationRecord
   def update_with_mentions(params)
     success = false
     ActiveRecord::Base.transaction do
-      raise ActiveRecord::Rollback unless update(params)
-      raise ActiveRecord::Rollback unless update_mentions
-
-      success = true
+      success = update(params)
+      success &= save_mentions
+      raise ActiveRecord::Rollback unless success
     end
     success
   end
 
-  def update_mentions
+  def save_mentions
     mention_success = true
 
     before_ids = mentioning_reports.ids
-    after_ids = find_ids
+    after_ids = content.scan(%r{http://localhost:3000/reports/(\d+)}).uniq.flatten.map(&:to_i)
 
     to_destroy_mentioned_ids = before_ids - after_ids
     Mention.where(mentioning_report_id: id, mentioned_report_id: to_destroy_mentioned_ids).find_each do |m|
-      mention_success = false unless m.destroy
+      mention_success &= m.destroy
     end
 
     to_create_mentioned_ids = after_ids - before_ids
-    to_create_mentioned_ids.each do |mentioned_id|
-      mention_success = false unless Mention.create(mentioning_report_id: id, mentioned_report_id: mentioned_id)
-    end
+    mention_success &= to_create_mentioned_ids.all? { |mentioned_id| Mention.create(mentioning_report_id: id, mentioned_report_id: mentioned_id) }
     mention_success
   end
 end
