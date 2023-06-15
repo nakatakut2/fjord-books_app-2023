ef2a920d90cdbf4cda0a791408bd3fb9eb07fce8
nakatakut2 <nakatakut2@gamil.com>
2023-06-15 17:37:47 +0900

    変数を使わないように修正

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -5,13 +5,13 @@ require 'application_system_test_case'
 class ReportsTest < ApplicationSystemTestCase
   setup do
     visit new_user_session_url
-    fill_in 'Eメール', with: 'user1@test.com'
+    fill_in 'Eメール', with: 'alice@example.com'
     fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
 
     assert_text 'ログインしました。'
 
-    @report = reports(:report_by_user1)
+    @report = reports(:report_by_alice)
   end
 
   test 'visiting the index' do
@@ -23,13 +23,13 @@ class ReportsTest < ApplicationSystemTestCase
     visit reports_url
     click_on '日報の新規作成'
 
-    fill_in 'タイトル', with: @report.title
-    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: "I'm alice."
+    fill_in '内容', with: 'Nice to meet you.'
     click_on '登録する'
 
     assert_text '日報が作成されました。'
-    assert_text @report.title
-    assert_text @report.content
+    assert_text "I'm alice."
+    assert_text 'Nice to meet you.'
 
     click_on '日報の一覧に戻る'
     assert_selector 'h1', text: '日報の一覧'
@@ -39,13 +39,13 @@ class ReportsTest < ApplicationSystemTestCase
     visit report_url(@report)
     click_on 'この日報を編集', match: :first
 
-    fill_in 'タイトル', with: '編集します'
-    fill_in '内容', with: 'この日報を編集します'
+    fill_in 'タイトル', with: '祖母のお祝いで親族集結'
+    fill_in '内容', with: '祖母の元気な顔が見れてよかったけど、普段会わない親族もいて変に気を遣ってしまった(笑)'
     click_on '更新する'
 
     assert_text '日報が更新されました。'
-    assert_text '編集します'
-    assert_text 'この日報を編集します'
+    assert_text '祖母のお祝いで親族集結'
+    assert_text '祖母の元気な顔が見れてよかったけど、普段会わない親族もいて変に気を遣ってしまった(笑)'
 
     click_on '日報の一覧に戻る'
     assert_selector 'h1', text: '日報の一覧'
@@ -56,6 +56,9 @@ class ReportsTest < ApplicationSystemTestCase
     click_on 'この日報を削除', match: :first
 
     assert_text '日報が削除されました。'
+    assert_no_text '祖母のお祝いで親族集結'
+    assert_no_text '祖母の元気な顔が見れてよかったけど、普段会わない親族もいて変に気を遣ってしまった(笑)'
+
     assert_selector 'h1', text: '日報の一覧'
   end
 end
