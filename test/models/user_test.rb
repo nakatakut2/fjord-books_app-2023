2e9d8226d865abf6e86ae064a038b484dfc416c8
nakatakut2 <nakatakut2@gamil.com>
2023-06-15 17:39:01 +0900

    テスト方法をUse.newに統一

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,14 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  setup do
-    @user = users(:user1)
-  end
-
   test 'name_or_email' do
-    user = User.new(email: 'test@test.com', name: '')
-    assert_equal 'test@test.com', user.name_or_email
+    user = User.new(email: 'test@example.com', name: '')
+    assert_equal 'test@example.com', user.name_or_email
 
-    assert_equal 'user1', @user.name_or_email
+    user = User.new(email: 'test@example.com', name: 'test')
+    assert_equal 'test', user.name_or_email
   end
 end
