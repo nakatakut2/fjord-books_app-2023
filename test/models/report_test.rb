30140612860b1d77211c221b4c22fa7e37b69d60
nakatakut2 <nakatakut2@gamil.com>
2023-06-15 17:50:29 +0900

    test 'save_mention'の修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,37 +4,41 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   setup do
-    @user1 = users(:user1)
-    @user2 = users(:user2)
-    @report_by_user1 = reports(:report_by_user1)
-    @report_by_user2 = reports(:report_by_user2)
-    @report_by_user3 = reports(:report_by_user3)
-    @report_to_mention_by_user4 = reports(:report_to_mention_by_user4)
+    @alice = users(:alice)
+    @bob = users(:bob)
+    @report_by_alice = reports(:report_by_alice)
+    @report_by_bob = reports(:report_by_bob)
+    @report_by_carol = reports(:report_by_carol)
   end
 
   test 'editable?' do
-    assert @report_by_user1.editable?(@user1)
-    assert_not @report_by_user1.editable?(@user2)
+    assert @report_by_alice.editable?(@alice)
+    assert_not @report_by_alice.editable?(@bob)
   end
 
   test 'created_on' do
-    assert_not_equal Date.parse('Fri, 30 Jnue 2023'), @report_by_user1.created_on
-    assert_equal Date.parse('Tue, 30 May 2023'), @report_by_user1.created_on
+    assert_not_equal Date.parse('Fri, 30 Jnue 2023'), @report_by_alice.created_on
+    assert_equal Date.current, @report_by_alice.created_on
   end
 
   test 'save_mentions' do
     # URLを含めないで日報を作成
-    assert_equal [], @report_by_user1.send(:save_mentions)
+    assert_equal [], @report_by_alice.trigger_save_mentions
 
     # URLを含めて日報を作成
-    assert_equal [@report_by_user1, @report_by_user2].sort, @report_to_mention_by_user4.send(:save_mentions).sort
+    report_with_mention = Report.create(
+      user: users(:dave),
+      title: 'I mention to you.',
+      content: "I mention to http://localhost:3000/reports/#{@report_by_alice.id} and http://localhost:3000/reports/#{@report_by_bob.id}."
+    )
+    assert_equal [@report_by_alice, @report_by_bob].sort, report_with_mention.mentioning_reports.sort
 
     # URLを変更して日報を更新
-    @report_to_mention_by_user4.update(content: 'I mention to http://localhost:3000/reports/1 and http://localhost:3000/reports/3.')
-    assert_equal [@report_by_user1, @report_by_user3].sort, Report.find(@report_to_mention_by_user4.id).send(:save_mentions).sort
+    report_with_updated_mention = Report.update(report_with_mention.id, content: "I mention to http://localhost:3000/reports/#{@report_by_alice.id} and http://localhost:3000/reports/#{@report_by_carol.id}.")
+    assert_equal [@report_by_alice, @report_by_carol].sort, report_with_updated_mention.mentioning_reports.sort
 
     # URLを削除して日報を更新
-    @report_to_mention_by_user4.update(content: 'I do not tmention.')
-    assert_equal [], Report.find(@report_to_mention_by_user4.id).send(:save_mentions)
+    report_with_mention.update(content: 'I do not mention.')
+    assert_equal [], Report.find(report_with_mention.id).trigger_save_mentions
   end
 end
