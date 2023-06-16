d47d6e4baea7f94b90b5b9c1a8f7c503b2bc5cbf
nakatakut2 <nakatakut2@gamil.com>
2023-06-16 14:24:56 +0900

    アサーションの実値をmentioning_reportsに統一

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -17,28 +17,38 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test 'created_on' do
-    assert_not_equal Date.parse('Fri, 30 Jnue 2023'), @report_by_alice.created_on
-    assert_equal Date.current, @report_by_alice.created_on
+    @report_by_alice.created_at = '2023-06-16 13:00'.in_time_zone
+    assert_equal '2023-06-16'.to_date, @report_by_alice.created_on
   end
 
   test 'save_mentions' do
     # URLを含めないで日報を作成
-    assert_equal [], @report_by_alice.trigger_save_mentions
+    params = {
+      user: users(:alice),
+      title: 'I wrote the second report.',
+      content: 'The weather is nice today.'
+    }
+    new_report = Report.create(params)
+    assert_equal [], new_report.mentioning_reports
 
     # URLを含めて日報を作成
-    report_with_mention = Report.create(
+    params = {
       user: users(:dave),
       title: 'I mention to you.',
       content: "I mention to http://localhost:3000/reports/#{@report_by_alice.id} and http://localhost:3000/reports/#{@report_by_bob.id}."
-    )
+    }
+    report_with_mention = Report.create(params)
     assert_equal [@report_by_alice, @report_by_bob].sort, report_with_mention.mentioning_reports.sort
 
     # URLを変更して日報を更新
-    report_with_updated_mention = Report.update(report_with_mention.id, content: "I mention to http://localhost:3000/reports/#{@report_by_alice.id} and http://localhost:3000/reports/#{@report_by_carol.id}.")
-    assert_equal [@report_by_alice, @report_by_carol].sort, report_with_updated_mention.mentioning_reports.sort
+    params = {
+      content: "I mention to http://localhost:3000/reports/#{@report_by_alice.id} and http://localhost:3000/reports/#{@report_by_carol.id}."
+    }
+    report_with_mention.update(params)
+    assert_equal [@report_by_alice, @report_by_carol].sort, report_with_mention.reload.mentioning_reports.sort
 
     # URLを削除して日報を更新
     report_with_mention.update(content: 'I do not mention.')
-    assert_equal [], Report.find(report_with_mention.id).trigger_save_mentions
+    assert_equal [], report_with_mention.reload.mentioning_reports
   end
 end
